ab48ffd918ed50438436f7ee70c6a6176ea96ddf
kzarka <nhtienptit@gmail.com>
2019-04-17 20:48:44 +0700

    a

--- convert.js
+++ convert.js
@@ -2,6 +2,7 @@ const data = require("./data/dependencies/war/members.json");
 const membersModel = require("./core/sqllite/members.js");
 for(let x in data) {
 	data[x].userId = x;
+	data[x].character = data[x].main;
 	membersModel.insert(data[x]);
 }
 console.log(data);
