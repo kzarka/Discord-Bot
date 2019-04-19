c206fd5ee53efb78e794efaec0fef214ae2e6efc
Huy Tien <nhtienptit@gmail.com>
2019-04-19 16:15:28 +0700

    a

--- helper/roles.js
+++ helper/roles.js
@@ -5,37 +5,37 @@ var helper = {
 }
 
 helper.reAsignRole = function(message, roleName) {
-    let roleToKeep = ['VIP', 'Filthy Outsider', 'Carrying Fury Scrubs', 'Guest', 'Ng0k', '( ͡° ͜ʖ ͡°)', roleName];
+    let roleToKeep = ['VIP', 'Filthy Outsider', 'Carrying Fury Scrubs', 'Guest', 'Ng0k', '( ͡° ͜ʖ ͡°)'];
+    // first remove old role
+    let isOfficer = roles.some(role => role.name === 'Officer');
+    if(!isOfficer) {
+        roles = roles.filter(function(role) {
+            if(!roleToKeep) return true;
+            if(roleToKeep.constructor !== Array) {
+                return role.name !== roleToKeep;
+            }
+            let result = true;
+            for(let i in roleToKeep) {
+                if(role.name == roleToKeep[i]){
+                    result = false;
+                }
+            }
+            return result;
+        });
+        message.member.removeRoles(roles).catch(err => {
+            console.log(err);
+        });
+    }
+
+    /* Now add role to member */
     let roles = message.member.roles;
     let role = message.guild.roles.find(r => r.name === roleName);
     if(!role) return;
     if(!message.member.roles.has(role.id)) {
-        console.log('Not has this role');
-        console.log(role.name);
         message.member.addRole(role).catch((e) => {
             console.log(e);
         });
     }
-    /* Now add role to member */
-    
-    let isOfficer = roles.some(role => role.name === 'Officer');
-    if(isOfficer) return;
-    roles = roles.filter(function(role) {
-        if(!roleToKeep) return true;
-        if(roleToKeep.constructor !== Array) {
-            return role.name !== roleToKeep;
-        }
-        let result = true;
-        for(let i in roleToKeep) {
-            if(role.name == roleToKeep[i]){
-                result = false;
-            }
-        }
-        return result;
-    });
-    message.member.removeRoles(roles).catch(err => {
-        console.log(err);
-    });
 }
 
 module.exports = helper;
