c130ba2a115013c751671b25aafdce18bb4d8830
tien.nguyen <tien.nguyen3@ntq-solution.com.vn>
2019-10-10 10:44:19 +0700

    Random response

--- dependencies/autochat.js
+++ dependencies/autochat.js
@@ -4,11 +4,13 @@ const axios = require("axios");
 
 module.exports = function(client){
 
+	var hi = ['', 'Hi', 'Hello', 'Sup', 'Yo', 'Whassup', '?'];
 	client.on('message', async message => {
 		let mentionUser = message.mentions.users.first();
 		if(!mentionUser || mentionUser.id != client.user.id) return;
 		let msgContent = message.content;
-		msgContent = msgContent.replace(`<@${mentionUser.id}>`, "");
+		msgContent = msgContent.replace(`<@${mentionUser.id}>`, "").trim();
+		if(msgContent == '') return message.channel.send(hi[Math.floor(Math.random() * (hi.length - 1)) + 1])
 		axios.get('http://api.minhhieu.asia/vi.php',{params:{text:msgContent}})
       		.then( response =>{
         		message.channel.send(response.data);
