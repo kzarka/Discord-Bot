1b0057a0e7c7060efe5ae5ae4ba7e8a9e52ad6d8
Kzarka <nhtienptit@gmail.com>
2019-12-15 22:55:12 +0700

    Chat

--- dependencies/autochat.js
+++ dependencies/autochat.js
@@ -12,7 +12,7 @@ module.exports = function(client){
 		msgContent = msgContent.replace(`<@${mentionUser.id}>`, "").trim();
 		if(msgContent == '') return message.channel.send(hi[Math.floor(Math.random() * (hi.length - 1)) + 1]);
 		message.channel.startTyping();
-		axios.get('http://bdovietnam.com/sisimi/sim.php',{params:{text:msgContent}})
+		axios.get('https://bdovietnam.com/api/simsimi/chat',{params:{text:msgContent}})
       		.then( response =>{
       			message.channel.stopTyping();
       			let responseMsg = 'Em bị lỗi rồi';
