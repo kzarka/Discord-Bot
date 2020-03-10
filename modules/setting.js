1adb79d278e0fe12ec181a6aa4ff8dad7c442e04
tien.nguyen <tien.nguyen3@ntq-solution.com.vn>
2020-03-10 18:20:12 +0700

    Add setting

---
+++ modules/setting.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var modules = {
+	description: 'Music module'
+};
+
+const dataDir = '/data/modules/setting/';
+
+modules.set = function(client, message, args) {
+	if(args.length == 0) return;
+	if(args[0] == 'channel') {
+		message.channel.send('Được chọn là channel chính.');
+	}
+};
+
+module.exports = modules;
