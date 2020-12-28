f88742ffd2da1b88cf3f0d03ddfaef1d4cb1e5b3
tien.nguyen <tien.nguyen3@ntq-solution.com.vn>
2020-12-28 19:33:06 +0700

    Fix

--- modules/member.js
+++ modules/member.js
@@ -32,7 +32,8 @@ async function getUserData(client, message) {
 		let userData = client.guildsData[message.guild.id].members[user.id];
 		let attachment = await drawImage(user, userData);
 		message.channel.send(attachment);
-	} catch {
+	} catch(e) {
+		console.log(e);
 		message.channel.send('Thành viên này chưa báo danh!');
 	}
 }
@@ -124,34 +125,78 @@ function buildListUser(list, message, buildAll = true, page = 0, totalPage = 0) 
 module.exports = modules;
 
 async function drawImage(member, userData) {
-	const canvas = Canvas.createCanvas(900, 250);
+	const canvas = Canvas.createCanvas(857, 238);
 	const ctx = canvas.getContext('2d');
 
-	const background = await Canvas.loadImage('.' + dataDir + 'background.png');
+	const background = await Canvas.loadImage(getClassBg(userData.class));
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-	ctx.strokeStyle = '#74037b';
+	let memberTag = member.user.tag;
+	let memberName = memberTag.split('#')[0];
+	let memberId = '#' + memberTag.split('#')[1];
+
+	//ctx.fillStyle = "rgba(240, 238, 246, 0.7)";
+	roundRect(ctx, 240, 80, 600, 120, 5, "rgba(9, 10, 11, 0.8)");
+
+	ctx.font = '16px Roboto';
+	ctx.fillStyle = '#62d3f5';
+	ctx.fillText(`FAMILY `, 250, 60);
+
+	//ctx.strokeStyle = 'black';
 	ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
+	// RANK SECTION
+	roundRect(ctx, 240, 30, 600, 40, 5, "rgba(9, 10, 11, 0.9)");
+
+	ctx.font = '28px Roboto';
+	ctx.fillStyle = '#62d3f5';
+	let lengthName = ctx.measureText(memberName).width;
+
+	ctx.fillText(`${memberName}`, 250, 60);
+
+	ctx.font = '16px Roboto';
+	ctx.fillStyle = '#7f8384';
+	let lengthName2 = ctx.measureText(memberId).width;
+	ctx.fillText(`${memberId}`, 250+lengthName, 60);
+	
+	lengthName = lengthName+lengthName2 + 20;
+
+	ctx.font = '16px Roboto';
+	ctx.fillStyle = '#62d3f5';
+	lengthName2 = ctx.measureText('LEVEL').width;
+	ctx.fillText(`LEVEL `, 250+lengthName, 60);
+
+	lengthName = lengthName+lengthName2;
+
+	ctx.font = '28px Roboto';
+	ctx.fillStyle = 'white';
+	ctx.fillText(`${userData.level}`, 250+lengthName, 60);
+
 	// Slightly smaller text placed above the member's display name
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
+	ctx.font = '28px Roboto';
+	ctx.fillStyle = '#000000';
 	ctx.fillText(`${member.user.tag}:`, canvas.width / 2.5, canvas.height / 2.5);
 
 	// Add an exclamation point here and below
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`Class: ${userData.class}\nFamily/Character: ${userData.family}/${userData.character}\nLevel: ${userData.level}`, canvas.width / 2.5, canvas.height / 1.8);
+	ctx.font = '28px Roboto';
+
+	ctx.shadowColor="#000000";
+	ctx.shadowBlur=2;
+	//ctx.lineWidth=2;
+	//ctx.strokeText(`${userData.family}/${userData.character} (Lvl ${userData.level})\nGear: ${userData.ap}/${userData.awk}/${userData.dp}`, canvas.width / 2.5, canvas.height / 1.8);
+	//ctx.shadowBlur=0;
+	ctx.fillStyle = '#000000';
+	ctx.fillText(`${userData.family}/${userData.character} (Lvl ${userData.level})\nGear: ${userData.ap}/${userData.awk}/${userData.dp}`, canvas.width / 2.5, canvas.height / 1.8);
 
 	ctx.beginPath();
 	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.clip();
-	console.log(member.user);
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	//const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	let avatar = await Canvas.loadImage('https://cdn.discordapp.com/avatars/213722448070180864/187bbc9ae2f6538f03a641f12596bf3c.png');
 	ctx.drawImage(avatar, 25, 25, 200, 200);
 
-	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'member-info.png');
 
 	return attachment;
 }
@@ -171,3 +216,46 @@ const applyText = (canvas, text) => {
 	// Return the result to use in the actual canvas
 	return ctx.font;
 };
+
+function getClassBg(className) {
+	if(!className) return '.' + dataDir + 'background.png';
+	console.log('./images/classes/' + className.toLowerCase().replace(' ', '_') + '.jpg');
+	return './images/classes/' + className.toLowerCase().replace(' ', '_') + '.jpg';
+}
+
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+	if (typeof stroke === 'undefined') {
+		stroke = true;
+	}
+	if (typeof radius === 'undefined') {
+		radius = 5;
+	}
+	if (typeof radius === 'number') {
+		radius = {tl: radius, tr: radius, br: radius, bl: radius};
+	} else {
+		var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+		for (var side in defaultRadius) {
+	  		radius[side] = radius[side] || defaultRadius[side];
+		}
+	}
+
+	ctx.beginPath();
+	ctx.moveTo(x + radius.tl, y);
+	ctx.lineTo(x + width - radius.tr, y);
+	ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+	ctx.lineTo(x + width, y + height - radius.br);
+	ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+	ctx.lineTo(x + radius.bl, y + height);
+	ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+	ctx.lineTo(x, y + radius.tl);
+	ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+	ctx.closePath();
+	if (fill) {
+		ctx.fillStyle = fill;
+		ctx.fill();
+	}
+	if (stroke) {
+		ctx.stroke();
+	}
+
+}
